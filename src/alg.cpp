3a301b8ef807f3cd0a733c19c59a57436bb72d09
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-05-12 21:17:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,24 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<std::string> tree;
+std::ifstream file(filename);
+std::string simb = "";
+if (!file) {
+  std::cout << "Error!" << std::endl;
+  return;
+}
+while (!file.eof()) {
+  simb = "";
+  while (true) {  
+    char dop = file.get();
+    if ((dop >= 65 && dop <= 90) || (dop >= 97 && dop <= 122)) {
+    simb += tolower(dop); //уменьш шр
+    } else {
+      break;
+  }
+}
+tree.add(simb);
+}
+return tree;
 }

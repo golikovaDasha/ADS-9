870d2dc570dd5869757e988a7e98c9b3639addd0
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-05-12 21:31:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,16 +6,16 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-BST<std::string> tree;
-std::ifstream file(filename);
-std::string simb = "";
-if (!file) {
-  std::cout << "Error!" << std::endl;
-  return;
+  BST<std::string> tree;
+  std::ifstream file(filename);
+  std::string simb = "";
+  if (!file) {
+    std::cout << "Error!" << std::endl;
+    return;
 }
 while (!file.eof()) {
   simb = "";
-  while (true) {  
+  while (true) {
     char dop = file.get();
     if ((dop >= 65 && dop <= 90) || (dop >= 97 && dop <= 122)) {
     simb += tolower(dop); //уменьш шр
